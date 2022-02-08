790e8e541f3b5eb7956e4071a6780c911a79fffa
xiaowei guan <xiaowei.guan@samsung.com>
2022-02-09 04:52:04 +0800

    Add video player class

--- packages/video_player/video_player/linux/include/video_player/video_player.h
+++ packages/video_player/video_player/linux/include/video_player/video_player.h
@@ -1,12 +1,28 @@
 #ifndef FLUTTER_PLUGIN_VIDEO_PLAYER_H_
 #define FLUTTER_PLUGIN_VIDEO_PLAYER_H_
 
+#include <flutter_linux/flutter_linux.h>
+
 typedef struct _VideoPlayer VideoPlayer;
 
 typedef struct {
   GObjectClass parent_class;
 } VideoPlayerClass;
 
-GType video_player_plugin_get_type();
+GType video_player_get_type();
+
+int video_player_play(VideoPlayer* player);
+
+void video_player_pause(VideoPlayer* player);
+
+void video_player_set_looping(VideoPlayer* player, bool is_looping);
+
+void video_player_set_volume(VideoPlayer* player, double volume);
+
+void video_player_set_play_back_speed(VideoPlayer* player, double speed);
+
+void video_player_seek(VideoPlayer* player, int position);
+
+int video_player_get_position(VideoPlayer* player);
 
 #endif
--- packages/video_player/video_player/linux/video_player.cc
+++ packages/video_player/video_player/linux/video_player.cc
@@ -1,28 +1,39 @@
-#include "video_player.h"
+#include "include/video_player/video_player.h"
 
-#define VIDEO_PLAYER_(obj)                                     \
-  (G_TYPE_CHECK_INSTANCE_CAST((obj), video_player_get_type(), \
-                              VideoPlayer))
+#include <vlc/vlc.h>
+
+#define VIDEO_PLAYER_(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST((obj), video_player_get_type(), VideoPlayer))
 
 struct _VideoPlayer {
   GObject parent_instance;
-  GHashTable* video_player_table;
+  libvlc_instance_t* instance;
+  libvlc_media_t* media;
+  libvlc_media_player_t* meida_player;
 };
 
 G_DEFINE_TYPE(VideoPlayer, video_player, g_object_get_type())
 
-static void video_player_dispose(GObject* object) {
-  FlTextureRegistrarImpl* self = FL_TEXTURE_REGISTRAR_IMPL(object);
-  g_clear_pointer(&self->textures, g_hash_table_unref);
-  G_OBJECT_CLASS(video_player_parent_class)->dispose(object);
-}
+static void video_player_dispose(GObject* object) {}
 
-static void video_player_init(VideoPlayer* self) {
-    g_autoptr(GHashTable) video_player_table = self->video_player_table;
-    self->video_player_table = g_hash_table_new_full(g_direct_hash, g_direct_equal, nullptr,
-                                         g_object_unref);
-}
+static void video_player_init(VideoPlayer* self) {}
 
 static void video_player_class_init(VideoPlayerClass* klass) {
   G_OBJECT_CLASS(klass)->dispose = video_player_dispose;
 }
+
+int video_player_play(VideoPlayer* player) {
+  return libvlc_media_player_play(player->meida_player);
+}
+
+void video_player_pause(VideoPlayer* player) {}
+
+void video_player_set_looping(VideoPlayer* player, bool is_looping) {}
+
+void video_player_set_volume(VideoPlayer* player, double volume) {}
+
+void video_player_set_play_back_speed(VideoPlayer* player, double speed) {}
+
+void video_player_seek(VideoPlayer* player, int position) {}
+
+int video_player_get_position(VideoPlayer* player) { return 0; }
